ed5e9b30ce30092fac8644907d990b2128f01d1c
faizih111 <eng.faizan11@gmail.com>
2025-08-16 13:02:00 +0500

    Create MiniProject_04.py

---
+++ MiniProject_04.py
@@ -0,0 +1,19 @@
+"---Student Information Organizer---"
+student1=("ali","18","12th Grade")
+student2=("ahmed","17","11th Grade")
+subjects={"English","Math","Physics"}
+my_dict={"ali":{
+ student1
+},
+"ahmed":{
+   student2
+}
+
+
+}
+print("Students: ",my_dict)
+print("Subjects: ",subjects)
+my_tuple=my_dict["ali"]
+print("Details for ali: ",my_tuple)
+subjects={"English","Math","Physics","English"}
+print("Subjects after adding duplicates: ",subjects)
