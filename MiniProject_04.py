3ded37bdbe421b440b6dded33a28052835fdc800
faizih111 <eng.faizan11@gmail.com>
2025-08-19 13:56:58 +0500

    update code

--- MiniProject_04.py
+++ MiniProject_04.py
@@ -1,19 +1,14 @@
 "---Student Information Organizer---"
-student1=("ali","18","12th Grade")
-student2=("ahmed","17","11th Grade")
-subjects={"English","Math","Physics"}
-my_dict={"ali":{
- student1
-},
-"ahmed":{
-   student2
-}
 
+student1 = ("ali", "18", "12th Grade")
+student2 = ("ahmed", "17", "11th Grade")
+subjects = {"English", "Math", "Physics"}
 
-}
-print("Students: ",my_dict)
-print("Subjects: ",subjects)
-my_tuple=my_dict["ali"]
-print("Details for ali: ",my_tuple)
-subjects={"English","Math","Physics","English"}
-print("Subjects after adding duplicates: ",subjects)
+my_dict = {"ali": {student1}, "ahmed": {student2}}
+print("Students: ", my_dict)
+print("Subjects: ", subjects)
+
+my_tuple = my_dict["ali"]
+print("Details for ali: ", my_tuple)
+subjects = {"English", "Math", "Physics", "English"}
+print("Subjects after adding duplicates: ", subjects)
